7e66eafc1680c0b879581b096d576b02db7b7094
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2023-06-23 01:23:02 +0300

    Дерево красно черное

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
+        tree.insert(15);
+        tree.insert(5);
+        tree.print();
+    }
+}
